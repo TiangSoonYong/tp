27fa0cc0bace5233be3a4cc0e0c6de3b8174a6e2
yyingg-243 <yying2243@gmail.com>
2025-04-08 13:39:17 +0800

    final check on checkstyle error

--- src/test/java/functions/ui/UITest.java
+++ src/test/java/functions/ui/UITest.java
@@ -11,7 +11,6 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.PrintStream;
-import game.Game;
 
 /**
  * A class contains Junit test cases for UI class
